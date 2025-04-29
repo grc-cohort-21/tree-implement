e7dc509b2182031ff737ee677b42f98d5e4504f7
Auberon López <auberon.lopez@greenriver.edu>
2025-04-29 18:49:35 +0000

    Day 1 livecode

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,28 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10, null, null);
+    root.left = new TreeNode(15);
+    root.left.left = new TreeNode(39);
+    root.left.right = new TreeNode(21);
 
+    root.right = new TreeNode(20);
+    root.right.left = new TreeNode(72);
+    root.right.left.right = new TreeNode(42);
+
+    postOrder(root);
+  }
+
+  public static void preOrder(TreeNode current) {
+    if(current == null) return;
+    System.out.println(current.value);
+    preOrder(current.left);
+    preOrder(current.right);
+  }
+
+  public static void postOrder(TreeNode current) {
+    if(current == null) return;
+    postOrder(current.left);
+    postOrder(current.right);
+    System.out.println(current.value);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,15 @@
 public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
 
+    public TreeNode(int value, TreeNode left, TreeNode right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
   
+    public TreeNode(int value) {
+        this(value, null, null);
+    }
 }
