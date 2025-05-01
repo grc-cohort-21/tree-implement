814bad9828c754494fbdf542a98a5b3ef651b28f
Auberon López <auberon.lopez@greenriver.edu>
2025-05-01 17:53:04 +0000

    Day2 part 1: generics

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,28 +1,46 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10, null, null);
-    root.left = new TreeNode(15);
-    root.left.left = new TreeNode(39);
-    root.left.right = new TreeNode(21);
+    TreeNode<Integer> root = new TreeNode<>(10, null, null);
+    root.left = new TreeNode<>(15);
+    root.left.left = new TreeNode<>(39);
+    root.left.right = new TreeNode<>(21);
 
-    root.right = new TreeNode(20);
-    root.right.left = new TreeNode(72);
-    root.right.left.right = new TreeNode(42);
+    root.right = new TreeNode<>(20);
+    root.right.left = new TreeNode<>(72);
+    root.right.left.right = new TreeNode<>(42);
 
-    postOrder(root);
+    TreeNode<String> stringRoot = new TreeNode<>("hello", null, null);
+    stringRoot.left = new TreeNode<>("cat");
+    stringRoot.left.left = new TreeNode<>("miku");
+    stringRoot.left.right = new TreeNode<>("dog");
+
+    stringRoot.right = new TreeNode<>("cyborg");
+    stringRoot.right.left = new TreeNode<>("jays");
+    stringRoot.right.left.right = new TreeNode<>("robocop");
+
+    inOrder(stringRoot);
   }
 
-  public static void preOrder(TreeNode current) {
+  public static void preOrder(TreeNode<?> current) {
     if(current == null) return;
     System.out.println(current.value);
     preOrder(current.left);
     preOrder(current.right);
   }
 
-  public static void postOrder(TreeNode current) {
+  public static <T> void postOrder(TreeNode<T> current) {
     if(current == null) return;
+    TreeNode<T> duplicate = new TreeNode<>(current.value);
+    T heldValue = current.value;
     postOrder(current.left);
     postOrder(current.right);
     System.out.println(current.value);
+  }
+
+  public static void inOrder(TreeNode<?> current) {
+    if(current == null) return;
+    inOrder(current.left);
+    System.out.println(current.value);
+    inOrder(current.right);
   }
 }
